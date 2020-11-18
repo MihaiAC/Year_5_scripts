1248ba9ea88d2ce1fcef4f75058cc7359ba7052c
MihaiAC <alexmihai.ciobanu@gmail.com>
2020-11-18 23:57:15 +0000

    Finish lab 6?.

--- TTDS/assignment2/code.py
+++ TTDS/assignment2/code.py
@@ -5,6 +5,7 @@ from nltk.stem.porter import PorterStemmer
 import numpy as np
 from typing import List, Set, Dict, Tuple, NewType
 from operator import itemgetter
+from nltk.corpus import stopwords
 
 class SimpleTokenizer():
     def __init__(self, pattern:str):
@@ -39,8 +40,10 @@ def construct_stopwords_set(stopwords_file_name:str) -> Set[str]:
         Set[str]: [description]
     """
     with open(stopwords_file_name, 'r') as f:
-        stopwords = f.read().splitlines()
-    return set(stopwords)
+        read_stopwords = f.read().splitlines()
+    stopwords_set = set(read_stopwords)
+    stopwords_set.update(stopwords.words("english"))
+    return stopwords_set
 
 class SimplePreprocessor():
     """Class for pre-processing text. Given a list of strings, it tokenizes them, removes stop words, lowercases and stems them.
@@ -70,7 +73,7 @@ class SimplePreprocessor():
 
 # ----------------------------------CREATE INDEX AND DOCID SET----------------------------------
 Index = NewType('Index', Dict[str, Dict[int, Dict[int, int]]])
-def read_corpora_and_create_index(corpora_file_names:List[str], preprocessor:SimplePreprocessor) -> Index:
+def read_corpora_and_create_index(corpora_file_names:List[str], preprocessor:SimplePreprocessor) -> Tuple[Index, Dict[int, int]]:
     """Reads input trec file and creates a positional inverted index from it, and it also creates a set containing all document IDs.
 
     Args:
@@ -78,15 +81,20 @@ def read_corpora_and_create_index(corpora_file_names:List[str], preprocessor:Sim
         preprocessor (SimplePreprocessor): initialized SimplePreprocessor.
     """
     index = dict()
-    for corpus_id, corpora_file_name in enumerate(corpora_file_names):
+    corpora_nr_docs = dict()
 
+    for corpus_id, corpora_file_name in enumerate(corpora_file_names):
+        corpora_nr_docs[corpus_id] = 0
         with open(corpora_file_name, 'r') as f:
             # Extract documents of the corpus.
             corpus = f.read()
-            documents = re.split(r"\n{2,}", corpus)
+            documents = re.split(r"\n\n", corpus)
             del corpus
             for (doc_id, doc) in enumerate(documents):
+                if len(doc) == 0:
+                    continue
                 doc_tokens = preprocessor.process_text_lines(doc.split("\n"))
+                corpora_nr_docs[corpus_id] += 1
                 for token in doc_tokens:
                     if token in index:
                         if corpus_id in index[token]:
@@ -105,7 +113,7 @@ def read_corpora_and_create_index(corpora_file_names:List[str], preprocessor:Sim
                     
         print("Index construction for corpus " + str(corpus_id+1) + " finished.")
 
-    return index
+    return index, corpora_nr_docs
 
 def calculate_freq_term(index:Index, term:str) -> int:
     if term not in index:
@@ -188,22 +196,15 @@ def compute_MI_chi_scores(index:Index, corpora_nr_docs:Dict[int, int], corpora_i
     
     for term in index:
         for corpus_id in corpora_ids:
+            N_11 = 0
             if corpus_id not in index[term]:
-                N_11 = 0
                 N_01 = corpora_nr_docs[corpus_id]
             else:
-                N_11 = 0
                 for _ in index[term][corpus_id]:
                     N_11 += 1
                 N_01 = corpora_nr_docs[corpus_id] - N_11
             N_10 = nr_docs_which_contain_term[term] - N_11
             N_00 = N - nr_docs_which_contain_term[term] - N_01
-
-            if term == "mother":
-                print(N_00)
-                print(N_01)
-                print(N_10)
-                print(N_11)
 
             MI_scores[corpus_id].append((term, compute_MI_score_term_corpus(N, N_00, N_01, N_10, N_11)))
             chi_scores[corpus_id].append((term, compute_chi_score_term_corpus(N, N_00, N_01, N_10, N_11)))
@@ -232,22 +233,63 @@ stopwords_set = construct_stopwords_set(stopwords_file_name)
 tokenizer = SimpleTokenizer('[a-zA-Z]+')
 stemmer = PorterStemmer()
 pre_processor = SimplePreprocessor(tokenizer, stopwords_set, stemmer)
-print('Pre-processing classes initialised.')
+
 
 # Create pos inverted index and the set of document IDs.
-# corpora_index = read_corpora_and_create_index(corpora_file_names, pre_processor)
+# corpora_index, corpora_nr_docs = read_corpora_and_create_index(corpora_file_names, pre_processor)
 # corpora_index = remove_low_freq_words_from_index(corpora_index, 10)
 # save_corpora_index(corpora_index, "corpora_index.pkl")
 
 # Warning: must take into account the fact that some documents may disappear.
 
-corpora_index = load_corpora_index("corpora_index.pkl")
-corpora_ids = [0, 1]
-corpora_nr_docs = calculate_nr_docs_per_corpus(corpora_index, corpora_ids)
-print(corpora_nr_docs)
+# corpora_index = load_corpora_index("corpora_index.pkl")
 
-MI_scores, chi_scores = compute_MI_chi_scores(corpora_index, corpora_nr_docs, corpora_ids)
-print("Corpus 1 MI:")
-print(MI_scores[0][:10])
-print("Corpus 1 chi: ")
-print(chi_scores[0][:10])
+
+# corpora_ids = [0, 1]
+# print(corpora_nr_docs)
+
+# MI_scores, chi_scores = compute_MI_chi_scores(corpora_index, corpora_nr_docs, corpora_ids)
+# print("Corpus 1 MI:")
+# print(MI_scores[0][:10])
+# print("Corpus 1 chi: ")
+# print(chi_scores[0][:10])
+
+
+from gensim.models.ldamodel import LdaModel
+from gensim.corpora.dictionary import Dictionary
+def get_corpus_topics(corpus_file_name:str, preprocessor:SimplePreprocessor):
+    with open(corpus_file_name, 'r') as f:
+        # Extract documents of the corpus.
+        corpus = f.read()
+        clean_docs = []
+        documents = re.split(r"\n\n", corpus)
+        for doc in documents:
+            clean_doc = preprocessor.process_text_lines(doc.split("\n"))
+            if len(clean_doc) > 0:
+                clean_docs.append(clean_doc)
+    
+    del documents
+    dictionary = Dictionary(clean_docs)
+    corpus = [dictionary.doc2bow(text) for text in clean_docs]
+    lda = LdaModel(corpus, num_topics=10, id2word=dictionary)
+    
+    total_prob = []
+    for ii in range(10):
+        total_prob.append([ii, 0])
+
+    nr_docs = len(clean_docs)
+    for doc in clean_docs:
+        doc_topics = lda.get_document_topics(dictionary.doc2bow(doc), 0)
+        for (topic_id, topic_prob) in doc_topics:
+            total_prob[topic_id][1] += topic_prob
+    
+    for ii in range(10):
+        total_prob[ii][1] /= nr_docs
+    
+    total_prob = sorted(total_prob, key=itemgetter(1), reverse=True)
+    
+    for (top_topic, probability) in total_prob[:3]:
+        print(str(top_topic) + " " + str(probability))
+        print(lda.print_topic(top_topic, 10))
+
+get_corpus_topics("corpus1.txt", pre_processor)
