53b760d1816d0eceeddcb88d20e14b93c734341c
MihaiAC <alexmihai.ciobanu@gmail.com>
2020-12-30 15:24:53 +0000

    Last contract modifications before testing and deployment.

--- BDL/CW_3/contracts/ERCiobToken.sol
+++ BDL/CW_3/contracts/ERCiobToken.sol
@@ -1,20 +1,19 @@
 pragma solidity >=0.4.22 <0.8.0;
 
-import SafeMath from "./SafeMath.sol"
-import IERCiobReceiver from "./IERCiobToken.sol"
+import "./SafeMath.sol";
+import "./IERCiobReceiver.sol";
 
 contract ERCiobToken {
     using SafeMath for uint256;
 
-    address public owner;
-
+    uint256 public transfer_timeout_period;
     uint256 public tokenPrice;
-    uint256 private nrSoldTokens;
-    mapping (address => uint256) owned_tokens;
-    uint8 timeout_period;
+    uint256 public nrSoldTokens;
     uint256 public last_price_change_block;
-
-
+    
+    address public owner;
+    mapping (address => uint256) owned_tokens;
+    
     event Purchase(address buyer, uint256 amount);
     event Transfer(address sender, address receiver, uint256 amount);
     event Sell(address seller, uint256 amount);
@@ -23,28 +22,29 @@ contract ERCiobToken {
     constructor() public {
         owner = msg.sender;
         tokenPrice = 5e14;
-        timeout_period = 5;
+        transfer_timeout_period = 25;
+        nrSoldTokens = 1;
 
         emit Price(tokenPrice);
     }
 
     function buyToken(uint256 amount) external payable returns(bool) {
         // Potential hazard: if buyer overflows amount... this is why SafeMath is used.
-        require(msg.value == amount.mul(tokenPrice), "Not enough funds.");
+        require(msg.value == amount.mul(tokenPrice), "Provided funds must exactly match the price of the bought tokens.");
         
-        owned_tokens[msg.sender].add(amount);
+        owned_tokens[msg.sender] = owned_tokens[msg.sender].add(amount);
+        nrSoldTokens = nrSoldTokens.add(amount);
+
         emit Purchase(msg.sender, amount);
         return true;
     }
 
-    // Need to somehow restrict this...instead of having a timeout period, you can assume that a sender is a 
-    // contract and call one of its functions to approve the transfer? (can state as an alternative)
     // Can user send tokens to 0x0? Yes, why not.
     function transfer(address recipient, uint256 amount) external returns(bool) {
-        require(block.number.sub(last_price_change_block) > timeout_period, "Not enough ");
+        require(block.number.sub(last_price_change_block) > transfer_timeout_period, "Token price has recently changed.");
 
-        owned_tokens[msg.sender].sub(amount);
-        owned_tokens[recipient].add(amount);
+        owned_tokens[msg.sender] = owned_tokens[msg.sender].sub(amount);
+        owned_tokens[recipient] = owned_tokens[recipient].add(amount);
 
         // Taken from OpenZeppelin's "Address" contract.
         uint256 size;
@@ -53,14 +53,15 @@ contract ERCiobToken {
             IERCiobReceiver receiver = IERCiobReceiver(recipient);
             receiver.receiveERCiobTokens(msg.sender, amount);
         }
-        
+
         emit Transfer(msg.sender, recipient, amount);
         return true;
-
     }
 
     function sellToken(uint256 amount) external returns(bool) {
-        owned_tokens[msg.sender].sub(amount);
+        owned_tokens[msg.sender] = owned_tokens[msg.sender].sub(amount);
+        nrSoldTokens = nrSoldTokens.sub(amount);
+
         msg.sender.transfer(amount.mul(tokenPrice));
 
         emit Sell(msg.sender, amount);
@@ -68,21 +69,26 @@ contract ERCiobToken {
     }
 
     function changePrice(uint256 price) external payable returns(bool) {
-        // Only the owner can call this method.
         require(msg.sender == owner, "Only the owner can call this function.");
         require(price >= tokenPrice.mul(2), "New token price is too low to warrant a change.");
-        require(address(this).balance.add(msg.value) > price.mul(nrSoldTokens), "Funds must suffice for the price increase.");
+        
+        uint256 contract_funds = address(this).balance;
+        uint256 tokens_value = price.mul(nrSoldTokens.sub(1));
+
+        require(contract_funds >= tokens_value, "Contract funds must cover the price increase.");
 
         tokenPrice = price;
         last_price_change_block = block.number;
+
+        if(contract_funds > tokens_value) {
+            msg.sender.transfer(contract_funds.sub(tokens_value));
+        }
 
         emit Price(price);
         return true;
     }
 
-    function getBalance() public returns(uint256) {
+    function getBalance() external view returns(uint256) {
         return owned_tokens[msg.sender];
     }
-
-    
 }
--- BDL/CW_3/contracts/SafeMath.sol
+++ BDL/CW_3/contracts/SafeMath.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity >=0.5.0 <0.8.0;
 
 /**
  * @dev Wrappers over Solidity's arithmetic operations with added overflow
@@ -100,60 +100,60 @@ library SafeMath {
      *
      * - The divisor cannot be zero.
      */
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        return div(a, b, "SafeMath: division by zero");
-    }
+    // function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    //     return div(a, b, "SafeMath: division by zero");
+    // }
 
-    /**
-     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
-     * division by zero. The result is rounded towards zero.
-     *
-     * Counterpart to Solidity's `/` operator. Note: this function uses a
-     * `revert` opcode (which leaves remaining gas untouched) while Solidity
-     * uses an invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     *
-     * - The divisor cannot be zero.
-     */
-    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        require(b > 0, errorMessage);
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    // /**
+    //  * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+    //  * division by zero. The result is rounded towards zero.
+    //  *
+    //  * Counterpart to Solidity's `/` operator. Note: this function uses a
+    //  * `revert` opcode (which leaves remaining gas untouched) while Solidity
+    //  * uses an invalid opcode to revert (consuming all remaining gas).
+    //  *
+    //  * Requirements:
+    //  *
+    //  * - The divisor cannot be zero.
+    //  */
+    // function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+    //     require(b > 0, errorMessage);
+    //     uint256 c = a / b;
+    //     // assert(a == b * c + a % b); // There is no case in which this doesn't hold
 
-        return c;
-    }
+    //     return c;
+    // }
 
-    /**
-     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
-     * Reverts when dividing by zero.
-     *
-     * Counterpart to Solidity's `%` operator. This function uses a `revert`
-     * opcode (which leaves remaining gas untouched) while Solidity uses an
-     * invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     *
-     * - The divisor cannot be zero.
-     */
-    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
-        return mod(a, b, "SafeMath: modulo by zero");
-    }
+    // /**
+    //  * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+    //  * Reverts when dividing by zero.
+    //  *
+    //  * Counterpart to Solidity's `%` operator. This function uses a `revert`
+    //  * opcode (which leaves remaining gas untouched) while Solidity uses an
+    //  * invalid opcode to revert (consuming all remaining gas).
+    //  *
+    //  * Requirements:
+    //  *
+    //  * - The divisor cannot be zero.
+    //  */
+    // function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    //     return mod(a, b, "SafeMath: modulo by zero");
+    // }
 
-    /**
-     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
-     * Reverts with custom message when dividing by zero.
-     *
-     * Counterpart to Solidity's `%` operator. This function uses a `revert`
-     * opcode (which leaves remaining gas untouched) while Solidity uses an
-     * invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     *
-     * - The divisor cannot be zero.
-     */
-    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        require(b != 0, errorMessage);
-        return a % b;
-    }
+    // /**
+    //  * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+    //  * Reverts with custom message when dividing by zero.
+    //  *
+    //  * Counterpart to Solidity's `%` operator. This function uses a `revert`
+    //  * opcode (which leaves remaining gas untouched) while Solidity uses an
+    //  * invalid opcode to revert (consuming all remaining gas).
+    //  *
+    //  * Requirements:
+    //  *
+    //  * - The divisor cannot be zero.
+    //  */
+    // function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+    //     require(b != 0, errorMessage);
+    //     return a % b;
+    // }
 }
